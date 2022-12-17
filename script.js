4f9982961ca7d1e60d2381b0c13ed05c586510a7
Annie  Indreiten <devannie777@gmail.com>
2022-12-17 15:34:21 +0000

    Contact form initial html and javaScript

--- script.js
+++ script.js
@@ -269,3 +269,77 @@ prevSlide.addEventListener("click", function () {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
+
+//Contact Form
+const form = document.querySelector("form[name='contact-form']");
+const nameInput = document.querySelector("input[name='name']");
+const emailInput = document.querySelector("input[name='email']");
+
+const messageInput = document.querySelector("textarea[name='message']");
+const thankYou = document.querySelector(".thank-you");
+
+console.log(form);
+
+nameInput.isValid = () => !!nameInput.value;
+emailInput.isValid = () => isValidEmail(emailInput.value);
+
+messageInput.isValid = () => !!messageInput.value;
+
+const inputFields = [nameInput, emailInput, messageInput];
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const isValidPhone = (phone) => {
+  const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(String(phone).toLowerCase());
+};
+
+let isFormValid = false;
+isValidationOn = false;
+
+const resetEl = (el) => {
+  el.classList.remove("invalid");
+  el.nextElementSibling.classList.add("hide");
+};
+
+const invalidateEl = (el) => {
+  el.classList.add("invalid");
+  el.nextElementSibling.classList.remove("hide");
+};
+
+const validateInputs = () => {
+  if (!isValidationOn) return;
+
+  isFormValid = true;
+  inputFields.forEach(resetEl);
+
+  if (!nameInput.value) {
+    isFormValid = false;
+    invalidateEl(nameInput);
+  }
+  if (!isValidEmail(emailInput.value)) {
+    isFormValid = false;
+    invalidateEl(emailInput);
+  }
+  if (!messageInput.value) {
+    isFormValid = false;
+    invalidateEl(messageInput);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  isValidationOn = true;
+
+  validateInputs();
+  if (isFormValid) {
+    form.remove();
+    thankYou.classList.remove("hide");
+  } // FOR THANK YOU msg may not need this*/
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
